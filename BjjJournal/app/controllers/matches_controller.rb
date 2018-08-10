b8f654a62e7576de63ecc2203e26361ff3aeabfc
David Yee <david.yee@bigbear.io>
2018-08-10 15:21:34 -0700

    belongs_to association is working

--- BjjJournal/app/controllers/matches_controller.rb
+++ BjjJournal/app/controllers/matches_controller.rb
@@ -20,7 +20,14 @@ class MatchesController < ApplicationController
     permitted_params = match_params
     @match.title=permitted_params[:title]
     @match.text=permitted_params[:text]
-    @match.competitor_id = Competitor.create!(:name=>permitted_params[:name], :height=>permitted_params[:height], :weight=>permitted_params[:weight]).id
+    competitor = Competitor.create(name: permitted_params[:name],
+                                   height: permitted_params[:height],
+                                   weight: permitted_params[:weight])
+
+
+    @match.competitor = competitor
+    puts "before save"
+    #@match.competitor_id = Competitor.create!(:name=>permitted_params[:name], :height=>permitted_params[:height], :weight=>permitted_params[:weight]).id
     @match.save!
     redirect_to @match
   end
--- BjjJournal/app/models/match.rb
+++ BjjJournal/app/models/match.rb
@@ -1,11 +1,15 @@
 class Match < ApplicationRecord
-
-  belongs_to :competitor, class_name: 'Competitor', foreign_key: 'competitor_id'
+  belongs_to :competitor
+  validates :title, :text, presence: true
   #before_save :create_competitor_from_input
 
-  def create_competitor_from_input(name, height, weight)
+  def create_competitor_from_input
+    puts "**************in create comp"
+    puts "name is #{name}"
+    puts "height is #{height}"
+    puts "weigth is #{weight}"
     #create_competitor(name: name, height: height, weight: weight)
-    Competitor.create!(:name => name, :height => height, :weight => weight).id
+    self.competitor_id=Competitor.create!(:name => name, :height => height, :weight => weight)
   end
 
 end
